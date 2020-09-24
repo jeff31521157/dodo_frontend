aa9979c5488fb545f3d6058af529cbc7411b0729
Zac <zac@contentos.io>
2020-09-24 11:59:19 +0800

    add entry on front page

--- pages/index.vue
+++ pages/index.vue
@@ -73,7 +73,7 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
+          <v-btn color="primary" nuxt to="/liquidity/COS-ETH"> Continue </v-btn>
         </v-card-actions>
       </v-card>
     </v-col>
